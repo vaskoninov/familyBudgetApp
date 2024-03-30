931bd8404a94485e6584efa78f048ddac7c14b90
vaskoninov <vasko.ninov@gmail.com>
2024-03-30 19:23:53 +0200

    added Tag creation functionality

--- familyBudgetApp/common/urls.py
+++ familyBudgetApp/common/urls.py
@@ -1,2 +1,7 @@
+from django.urls import path
+
+from familyBudgetApp.common import views
+
 urlpatterns = (
+    path("/create-new-tag/", views.CreateTagView.as_view(), name="create-new-tag"),
 )
--- familyBudgetApp/common/views.py
+++ familyBudgetApp/common/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.views import generic as views
+from django.urls import reverse_lazy
+
+from familyBudgetApp.common.mixins import RefererURLMixin
+from familyBudgetApp.common.models import Tag
+
 
 # Create your views here.
+
+class CreateTagView(RefererURLMixin, views.CreateView):
+    model = Tag
+    fields = ['name']
+    template_name = 'common/create-new-tag.html'
+    success_url = reverse_lazy('index')
